128b7842b59f3ce0d7bca22df7d08f1bef76c243
jxia3 <97870693+jxia3@users.noreply.github.com>
2024-11-05 13:04:40 -0500

    add train config

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import json
 import numpy as np
+from typing import Any
 
 from environment import GamblerGame
 from eval.evaluation import Evaluation
@@ -8,12 +10,30 @@ import rand
 # Game parameters
 TARGET_WEALTH: int = 20
 WIN_PROB: float = 0.4
-EVAL_EPISODES: int = 10000
+EVAL_EPISODES: int = 10_000
 SEED: int = 0
 
-# Initialize environment and train agent
+# Training configuration
+TRAIN_CONFIG: dict[str, Any] = {
+    "tabular_q": {
+        "train_fn": tabular_q.train,
+        "save_path": "data/tabular_q.json",
+    },
+    "deep_q": {
+        "train_fn": deep_q.train,
+        "save_path": "data/deep_q.json",
+    },
+}
+MODEL = "tabular_q"
+
+# Initialize environment and evaluation context
 rng = np.random.default_rng(seed=SEED)
 train_env = GamblerGame(TARGET_WEALTH, WIN_PROB, rand.generate_seed(rng))
 eval_env = GamblerGame(TARGET_WEALTH, WIN_PROB, rand.generate_seed(rng))
 evaluation = Evaluation(eval_env, EVAL_EPISODES, rand.generate_seed(rng))
-tabular_q.train(train_env, evaluation, rand.generate_seed(rng))
+
+# Train agent and save results
+config = TRAIN_CONFIG[MODEL]
+scores = config["train_fn"](train_env, evaluation, rand.generate_seed(rng))
+with open(config["save_path"], "w") as file:
+    json.dump(scores, file)
