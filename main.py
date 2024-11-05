a513b2149e23dd12a90ee833c25b6495f4a9b1a6
jxia3 <97870693+jxia3@users.noreply.github.com>
2024-11-04 19:16:24 -0500

    add train files

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from environment import GamblerGame
+import q_learning
+import policy_gradient
 
+import numpy as np
+
+# Game parameters
 TARGET_WEALTH: int = 20
 WIN_PROB: float = 0.5
 SEED: int = 0
+SEED_RANGE = (0, 1_000_000_000)
 
-env = GamblerGame(TARGET_WEALTH, WIN_PROB, SEED)
-state = env.reset()
+# Initialize environment and train agent
+rng = np.random.default_rng(seed=SEED)
+env = GamblerGame(TARGET_WEALTH, WIN_PROB, rng.integers(SEED_RANGE[0], SEED_RANGE[1]))
+q_learning.train(env, rng.integers(SEED_RANGE[0], SEED_RANGE[1]))
---
+++ q_learning.py
@@ -0,0 +1,4 @@
+from environment import GamblerGame, GamblerState
+
+def train(env: GamblerGame, seed: int):
+    """Trains a Deep Q-Learning agent on the gambler Markov decision process."""
