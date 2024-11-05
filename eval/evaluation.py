1c3ef17ea67dbea43d5d36cb401b34f01330edd5
jxia3 <97870693+jxia3@users.noreply.github.com>
2024-11-05 10:44:52 -0500

    add get action

--- eval/evaluation.py
+++ eval/evaluation.py
@@ -28,8 +28,28 @@ class Evaluation:
         """Evaluates the optimal policy."""
         return 0
 
+    def _get_q_table_action(self, q_table: np.ndarray, state: GamblerState) -> int:
+        """Queries an explicit Q-table for an action at a state."""
+        values = q_table[state.get_index()].copy()
+        values[~state.get_action_mask().numpy()] = -np.inf
+        return int(np.argmax(values))
+
+    def _get_q_table(self, model: nn.Module) -> np.ndarray:
+        """
+        Queries the model at each state to calculate the Q-table. For environments with large
+        state spaces or continuous states, storing the Q-table explicitly is intractable.
+        The gambler game has a small state space so we can represent the Q-table as a
+        2-dimensional array. Note that we only use the Q-table for efficient model evaluation.
+        """
+        q_table = np.zeros((self.env.get_state_size(), self.env.get_action_size()))
+        for state_index in range(1, self.env.get_state_size() - 1):
+            state = self.env.create_state(state_index)
+            print(state)
+
+        return np.array([])
+
     def _get_optimal_action(self, state: GamblerState) -> int:
-        """Computes the optimal action at a gambler game state."""
+        """Computes the optimal action at a state based on the win probability."""
         if self.env.win_prob >= 0.5:
             return 0
         return min(state.wealth, self.env.target_wealth - state.wealth) - 1
